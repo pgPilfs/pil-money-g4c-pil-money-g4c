1d5bfdb64bdd14e5bcfe7c0d1877baf98419a15e
lucasmartin007 <luesma102938475@hotmail.com>
2021-07-08 21:31:57 -0300

    typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+//router
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PIL - MONEY - GRUPO 4 AULA 4';
+
+  portada:boolean
+
+  constructor(
+    location: Location,
+    router: Router
+  ) {
+    router.events.subscribe((val) => {
+      if(location.path() === ''){
+        this.portada = true
+      } else {
+        this.portada = false
+      }
+    });
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
