7ccc851b5e83728accfb1f333e6ceec16c07bd09
Lauti Carracedo <lauti.carracedo@hotmail.com>
2021-07-22 16:52:49 -0300

    add servicio autenticacion user

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+import { tick } from '@angular/core/testing';
+import { Observable, of, Subject } from "rxjs";
+import { User } from "../models/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private users:User[];
+  private users$: Subject<User[]>;
+
+  constructor() {
+    this.users = [];
+    this.users$ = new Subject();
+   }
+
+   addUser(user: User){
+     this.users.push(user);
+     this.users$.next(this.users);
+
+   }
+
+   getUsers$():Observable<User[]>{
+     return this.users$.asObservable();
+   }
+}
