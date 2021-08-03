d5628273cb70d1819efb356ffa90322cc4c1a501
Lauti Carracedo <lauti.carracedo@hotmail.com>
2021-08-03 00:16:55 -0300

    add class servicio

---
+++ src/app/shared/models/service.ts
@@ -0,0 +1,24 @@
+export interface Service {
+    nombreServicio: string
+}
+
+export const Services: Service[] = [
+    {
+        nombreServicio: "Aguas Cordobesas"
+    },
+    {
+        nombreServicio: "EPEC"
+    },
+    {
+        nombreServicio: "Ecogas"
+    },
+    {
+        nombreServicio: "Telecom"
+    },
+    {
+        nombreServicio: "Cablevision"
+    },
+    {
+        nombreServicio: "CLARO"
+    }
+]
