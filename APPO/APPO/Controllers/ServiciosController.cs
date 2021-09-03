39c578d3ca70d3715b9ed9156122adfa237e04f9
Lauti Carracedo <lauti.carracedo@hotmail.com>
2021-09-03 12:44:01 -0300

    elimino proyecto back y empiezo uno nuevo (con servicios controller)

---
+++ APPO/APPO/Controllers/ServiciosController.cs
@@ -0,0 +1,65 @@
+﻿using APPO.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APPO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiciosController : ControllerBase
+    {
+        private readonly APPOContext _context;
+
+        public ServiciosController(APPOContext context)
+        {
+            _context = context;
+
+        }
+        // GET: api/<ServiciosController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listServicios = await _context.Servicios.ToListAsync();
+                return Ok(listServicios);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ServiciosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ServiciosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ServiciosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ServiciosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ APPO/APPO/Models/Servicio.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace APPO.Models
+{
+    public partial class Servicio
+    {
+
+        public class APPOContext : DbContext
+        {
+
+        }
+        public Servicio()
+        {
+            PagosServicios = new HashSet<PagosServicio>();
+        }
+
+        public int IdServicio { get; set; }
+        public string NombreServicio { get; set; }
+
+        public virtual ICollection<PagosServicio> PagosServicios { get; set; }
+    }
+}
