45944fe1fe843d0615ced0e87aad4c28f86c34b0
Lauti Carracedo <lauti.carracedo@hotmail.com>
2021-09-06 23:14:06 -0300

    enlazando back y front de ingreso dinero, aunque tira error 500

--- frontend/src/app/modules/pages/wallet/ingreso-money/ingreso-money.module.ts
+++ frontend/src/app/modules/pages/wallet/ingreso-money/ingreso-money.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { IngresoMoneyRoutingModule } from './ingreso-money-routing.module';
 import { IngresoMoneyComponent } from './ingreso-money.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [IngresoMoneyComponent],
   imports: [
     CommonModule,
-    IngresoMoneyRoutingModule
+    IngresoMoneyRoutingModule,
+    FormsModule
   ]
 })
 export class IngresoMoneyModule { }
---
+++ frontend/src/app/shared/models/ingreso.ts
@@ -0,0 +1,10 @@
+export class Ingreso {
+    id_deposito: number;
+    nombre_deposito: string;
+    CVU_deposito: string;
+    nro_tarjeta: string;
+    fecha_venc: Date;
+    cod_seguridad: number;
+    nombre_titular: string
+}
+
