5b6b03cfa9840345137ee6f7b4e695ac81f2cebd
Lauti Carracedo <lauti.carracedo@hotmail.com>
2021-09-16 09:15:37 -0300

    controlador cuenta

--- APPO 2.0/APPO 2.0/Controllers/CuentaController.cs
+++ APPO 2.0/APPO 2.0/Controllers/CuentaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using APPO_2._0.Models;
+using APPO_2._0.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,56 @@ namespace APPO_2._0.Controllers
     [ApiController]
     public class CuentaController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get(Cuenta oCuenta)
+        {
+            try
+            {
+                using (APPO20Context db = new APPO20Context())
+                {
+                    CuentaViewModel oModel = new CuentaViewModel();
+                    oModel.Cvu = oCuenta.Cvu;
+                    oModel.SaldoActual = oCuenta.SaldoActual;
+                    var lista = db.Cuentas.ToList();
+                    return Ok(lista);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Add(ServicioViewModel oModel)
+    {
+        Response oRespuesta = new Response();
+        oRespuesta.Exito = 0;
+        try
+        {
+            using (APPO20Context db = new APPO20Context())
+            {
+                Servicio oServicio = new Servicio();
+                oServicio.NombreServicio = oModel.NombreServicio;
+                db.Servicios.Add(oServicio);
+                db.SaveChanges();
+                oRespuesta.Exito = 1;
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            oRespuesta.Mensaje = ex.Message;
+        }
+
+        return Ok(oRespuesta);
+
+
     }
 }
