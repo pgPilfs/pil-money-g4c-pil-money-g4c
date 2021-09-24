27339380e2dad24eef185fcb54345a4d6c31e9ea
Lauti Carracedo <lauti.carracedo@hotmail.com>
2021-09-24 16:09:33 -0300

    pagos de servicios: controlar que la cuenta exista y que el saldo sea suficiente para pagar

--- APPO 2.0!/APPO 2.0!/Controllers/PagoServicioController.cs
+++ APPO 2.0!/APPO 2.0!/Controllers/PagoServicioController.cs
@@ -35,64 +35,70 @@ namespace APPO_2._0.Controllers
 
 
                 if (oModel.NombreFactura == "CABLEVISION")
-                    {
-                        oPagos.IdServicio = 1;
+                {
+                    oPagos.IdServicio = 1;
 
-                    }
-                    if (oModel.NombreFactura == "EPEC")
-                    {
-                        oPagos.IdServicio = 2;
+                }
+                if (oModel.NombreFactura == "EPEC")
+                {
+                    oPagos.IdServicio = 2;
 
-                    }
-                    if (oModel.NombreFactura == "AGUAS CORDOBESAS")
-                    {
-                        oPagos.IdServicio = 3;
+                }
+                if (oModel.NombreFactura == "AGUAS CORDOBESAS")
+                {
+                    oPagos.IdServicio = 3;
 
-                    }
-                    if (oModel.NombreFactura == "ECOGAS")
-                    {
-                        oPagos.IdServicio = 4;
+                }
+                if (oModel.NombreFactura == "ECOGAS")
+                {
+                    oPagos.IdServicio = 4;
 
-                    }
-                    if (oModel.NombreFactura == "RENTAS")
-                    {
-                        oPagos.IdServicio = 5;
+                }
+                if (oModel.NombreFactura == "RENTAS")
+                {
+                    oPagos.IdServicio = 5;
 
-                    }
-                    if (oModel.NombreFactura == "FIBERTEL")
-                    {
-                        oPagos.IdServicio = 6;
+                }
+                if (oModel.NombreFactura == "FIBERTEL")
+                {
+                    oPagos.IdServicio = 6;
 
-                    }
-                    if (oModel.NombreFactura == "TELECOM")
-                    {
-                        oPagos.IdServicio = 7;
+                }
+                if (oModel.NombreFactura == "TELECOM")
+                {
+                    oPagos.IdServicio = 7;
 
-                    }
+                }
 
-                    oPagos.CvuPago = oModel.CvuPago;
-                    oPagos.NroFactura = oModel.NroFactura;
-                    oPagos.NombreFactura = oModel.NombreFactura;
-                    oPagos.Monto = oModel.Monto;
-                    oPagos.Fecha = DateTime.Now;
+                oPagos.CvuPago = oModel.CvuPago;
+                oPagos.NroFactura = oModel.NroFactura;
+                oPagos.NombreFactura = oModel.NombreFactura;
+                oPagos.Monto = oModel.Monto;
+                oPagos.Fecha = DateTime.Now;
 
 
-                    decimal total = oModel.Monto;
+                decimal total = oModel.Monto;
 
+                if (cuenta == null)
+                {
+                    return BadRequest("Lo sentimos...Las cuenta de la que intentas realizar el pago no existe...");
+                }
+
+                if (cuenta.SaldoActual < total)
+                {
+                    return BadRequest("Lo sentimos... Tu cuenta no tiene el el dinero suficiente para realizar el pago");
+                }
+                else
+                {
                     cuenta.SaldoActual -= total;
                     _context.Cuentas.Update(cuenta);
+                }
 
-                    _context.PagosServicios.Add(oPagos);
+                _context.PagosServicios.Add(oPagos);
 
-                    await _context.SaveChangesAsync();
-                    return Ok(oPagos);
+                await _context.SaveChangesAsync();
+                return Ok(oPagos);
                     
-
-
-
-
-                
-
             }
             catch (Exception ex)
             {
